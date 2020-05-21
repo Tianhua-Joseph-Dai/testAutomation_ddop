d4db1433876f0439229d4e480032c59299e26727
Tianhua-Joseph-Dai <tianhuadai0312@gmail.com>
2020-05-21 09:28:03 +0200

    Edit with parametized test suite

--- src/main/java/com/test/testng/gameAutoLogin.java
+++ src/main/java/com/test/testng/gameAutoLogin.java
@@ -2,13 +2,15 @@ package com.test.testng;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class gameAutoLogin {
     @Test
-    public void autoLogin() {
-    System.setProperty("webdriver.chrome.driver", "tools/chromedriver.exe");
+    @Parameters({"driverPath", "url"})
+    public void autoLogin(String driverPath, String url) {
+    System.setProperty("webdriver.chrome.driver", driverPath);
         WebDriver webdriver = new ChromeDriver();
-        webdriver.get("https://www.google.de");
+        webdriver.get(url);
     }
 }
