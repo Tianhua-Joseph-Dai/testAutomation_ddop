535fd945dffb36e8977eaef5cc62f794d882a213
Tianhua-Joseph-Dai <tianhuadai0312@gmail.com>
2020-05-21 12:11:35 +0200

    Naruto game auto login

--- src/main/java/com/test/testng/gameAutoLogin.java
+++ src/main/java/com/test/testng/gameAutoLogin.java
@@ -1,16 +1,29 @@
 package com.test.testng;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Parameters;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+// import org.openqa.selenium.chrome.ChromeDriver;
 
 public class gameAutoLogin {
-    @Test
-    @Parameters({"driverPath", "url"})
-    public void autoLogin(String driverPath, String url) {
-    System.setProperty("webdriver.chrome.driver", driverPath);
-        WebDriver webdriver = new ChromeDriver();
-        webdriver.get(url);
+    @Test(dataProvider = "dataProvider")
+    public void autoLogin(String user_email, String user_pwd) throws InterruptedException {
+//        System.setProperty("webdriver.chrome.driver", "tools/chromedriver.exe");
+        System.setProperty("webdriver.gecko.driver", "tools/geckodriver.exe");
+        WebDriver webDriver = new FirefoxDriver();
+        webDriver.get("https://naruto.oasgames.com/en/serverlist/s763");
+        webDriver.findElement(By.name("user_email")).sendKeys(user_email);
+        webDriver.findElement(By.name("user_password")).sendKeys(user_pwd);
+        webDriver.findElement(By.xpath("//a[@onclick='ajax_login();']")).click();
+    }
+
+    @DataProvider
+//    @Parameters({"driverPath", "url"})
+    public Object[][] dataProvider() {
+        return new Object[][] {
+                new Object[] {"wiidai01@163.com", "wii1350"}
+        };
     }
 }
---
+++ src/main/java/com/test/testng/openGoogle.java
@@ -0,0 +1,16 @@
+package com.test.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class openGoogle {
+    @Test
+    @Parameters({"driverPath", "url"})
+    public void autoLogin(String driverPath, String url) {
+    System.setProperty("webdriver.chrome.driver", driverPath);
+        WebDriver webdriver = new ChromeDriver();
+        webdriver.get(url);
+    }
+}
